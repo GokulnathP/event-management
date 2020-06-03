b89d6a147ef701d8e6393e92ac44c0d8a268bebc
Gokulnath <4gokul2000@gmail.com>
2020-06-03 15:37:39 +0530

    Installing necessart packages and connect with mongodb

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+// External packages
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Variables to be used
 const app = express();
 
+// MongoDB And Server connection
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server started at ', PORT));
+mongoose.connect(
+    'mongodb+srv://event:testevent@cluster0-dsfbd.mongodb.net/eventmanagement?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => {
+    console.log('MongoDB Connected');
+    app.listen(PORT, () => console.log('Server started at ', PORT));
+}).catch(err => console.log('[MongoDB] ', err));
