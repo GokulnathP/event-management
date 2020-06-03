5f769b4944c08abd89dbe4a76d15a5664f8813f9
Gokulnath <4gokul2000@gmail.com>
2020-06-03 15:49:43 +0530

    User Schema created

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    otpData: {
+        otp: {
+            type: String,
+            require: true
+        },
+        expiresIn: {
+            type: Date,
+            require: true
+        }
+    }
+});
+
+
+module.exports = mongoose.model('User', userSchema);
