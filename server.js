2fb0c8343043448aa590bed9ac376290b499b559
Gokulnath <4gokul2000@gmail.com>
2020-06-03 15:45:23 +0530

    Setup middlewares for input and cors

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 // External packages
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Variables to be used
 const app = express();
+
+// Middlewares setup
+// Json input parsing
+app.use(bodyParser.json());
+
+// cors middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 // MongoDB And Server connection
 const PORT = process.env.PORT || 5000;
