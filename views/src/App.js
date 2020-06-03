e1857d51262bb1f45f3bf0b1a551d706649ba3c3
barathrajd <barathdeva757@gmail.com>
2020-06-03 15:05:13 +0530

    [Barathraj] Initial commit

--- views/src/App.js
+++ views/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input type="text" placeholder="Firstname" required /> <br />
+      <input type="text" placeholder="Lirstname" required /> <br />
+      <input type="email" placeholder="Email" name="" id="" required />
+      <br />
+      <input type="password" placeholder="Password" name="" id="" required />
+      <br />
+      <input
+        type="password"
+        min="6"
+        placeholder="Confrim Password"
+        name=""
+        id=""
+        required
+      />
+      <br />
+      <input type="button" value="Submit" className="btn btn-default" />
     </div>
   );
 }
