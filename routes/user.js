e83efd7784d25fdbbc767c53f4e3fc73783c2012
barathrajd <barathdeva757@gmail.com>
2020-08-17 19:04:57 +0530

    Git Pair

--- routes/user.js
+++ routes/user.js
@@ -10,42 +10,34 @@ const passwordController = require('../controllers/password');
 //Middleware
 const Auth = require('../middleware/auth');
 
-// post /user/register 
+// post /user/register
 router.post(
-    '/register',
-    [
-        body('email')
-            .isEmail()
-            .withMessage('Invalid E-mail'),
-        body('password')
-            .trim()
-            .isLength({ min: 5 })
-            .withMessage('Password must be at least 5 chars long')
-    ],
-    userController.register
+  '/register',
+  [
+    body('email').isEmail().withMessage('Invalid E-mail'),
+    body('password')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage('Password must be at least 5 chars long'),
+  ],
+  userController.register
 );
 
 // post /user/login
 router.post(
-    '/login',
-    [
-        body('email')
-            .isEmail()
-            .withMessage('Invalid E-mail'),
-        body('password')
-            .trim()
-            .isLength({ min: 5 })
-            .withMessage('Password must be at least 5 chars long')
-    ],
-    userController.login
+  '/login',
+  [
+    body('email').isEmail().withMessage('Invalid E-mail'),
+    body('password')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage('Password must be at least 5 chars long'),
+  ],
+  userController.login
 );
 
 // put /user/resetPassword/email
-router.put(
-    '/resetPassword/:email',
-    Auth,
-    passwordController.resetPassword
-);
+router.put('/resetPassword/:email', Auth, passwordController.resetPassword);
 
 // put /user/forgotPassword/email
 router.put('/forgotPassword/:email', passwordController.forgotPassword);
